6b2e0152f13f51fb6d9ca763f007e40fbdd1dd70
WebdevShefali <jangidshefali62@gmail.com>
2024-01-26 12:17:35 +0530

    updated api

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,70 +3,30 @@ import "leaflet/dist/leaflet.css";
 import * as L from "leaflet";
 import locIcon from "../images/icon-location.svg";
 import arrowIcon from "../images/icon-arrow.svg";
-import Swal from 'sweetalert2';
-// const api_key = process.env.REACT_APP_API_KEY;
-// console.log(api_key)
+import Swal from "sweetalert2";
+const api_key = process.env.REACT_APP_API_KEY;
+console.log(api_key);
 const Home = () => {
-  const url = `http://ip-api.com/json/`;
+  const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}`;
   const getData = async () => {
     const data = await fetch(url);
     const parsedData = await data.json();
-    document.querySelector(".ipAddDetail").innerHTML = parsedData.query;
+    document.querySelector(".ipAddDetail").innerHTML = parsedData.ip;
     document.querySelector(".locationDetail").innerHTML =
-      parsedData.city +
+      parsedData.location.city +
       ", " +
-      parsedData.country +
+      parsedData.location.country +
       ", " +
-      parsedData.regionName;
+      parsedData.location.region;
     document.querySelector(".timezoneDetail").innerHTML =
-    "UTC - " +   parsedData.timezone;
+      "UTC - " + parsedData.location.timezone;
     document.querySelector(".ispDetail").innerHTML = parsedData.isp;
     var container = L.DomUtil.get("map");
     if (container != null) {
       container._leaflet_id = null;
     }
     const map = L.map("map").setView(
-      [parsedData.lat, parsedData.lon],
-    13
-    );
-
-    const tiles = L.tileLayer(
-      "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        maxZoom: 35,
-      }
-    ).addTo(map);
-    var locationIcon = L.icon({
-      iconUrl: locIcon,
-      iconSize: [40, 45], // size of the icon
-    });
-
-    const marker = L.marker(
-      [parsedData.lat, parsedData.lon],
-      { icon: locationIcon }
-    ).addTo(map);
-  };
-  const handleClick = async () => {
-    try {
-        const ipAddInput = document.querySelector(".ipAddInput");
-    const url = `http://ip-api.com/json/${ipAddInput.value}`;
-    const data = await fetch(url);
-    const parsedData = await data.json();
-    document.querySelector(".ipAddDetail").innerHTML = ipAddInput.value;
-    document.querySelector(".locationDetail").innerHTML =
-      parsedData.city +
-      ", " +
-      parsedData.country +
-      ", " +
-      parsedData.regionName;
-    document.querySelector(".timezoneDetail").innerHTML = "UTC - " + parsedData.timezone;
-    document.querySelector(".ispDetail").innerHTML = parsedData.isp;
-    var container = L.DomUtil.get("map");
-    if (container != null) {
-      container._leaflet_id = null;
-    }
-    const map = L.map("map").setView(
-      [parsedData.lat, parsedData.lon],
+      [parsedData.location.lat, parsedData.location.lng],
       13
     );
 
@@ -82,17 +42,57 @@ const Home = () => {
     });
 
     const marker = L.marker(
-      [parsedData.lat, parsedData.lon],
+      [parsedData.location.lat, parsedData.location.lng],
       { icon: locationIcon }
     ).addTo(map);
+  };
+  const handleClick = async () => {
+    try {
+      const ipAddInput = document.querySelector(".ipAddInput");
+      const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${ipAddInput.value}`;
+      const data = await fetch(url);
+      const parsedData = await data.json();
+      document.querySelector(".ipAddDetail").innerHTML = parsedData.ip;
+      document.querySelector(".locationDetail").innerHTML =
+        parsedData.location.city +
+        ", " +
+        parsedData.location.country +
+        ", " +
+        parsedData.location.region;
+      document.querySelector(".timezoneDetail").innerHTML =
+        "UTC - " + parsedData.location.timezone;
+      document.querySelector(".ispDetail").innerHTML = parsedData.isp;
+      var container = L.DomUtil.get("map");
+      if (container != null) {
+        container._leaflet_id = null;
+      }
+      const map = L.map("map").setView(
+        [parsedData.location.lat, parsedData.location.lng],
+        13
+      );
+
+      const tiles = L.tileLayer(
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          maxZoom: 35,
+        }
+      ).addTo(map);
+      var locationIcon = L.icon({
+        iconUrl: locIcon,
+        iconSize: [40, 45], // size of the icon
+      });
+
+      const marker = L.marker(
+        [parsedData.location.lat, parsedData.location.lng],
+        { icon: locationIcon }
+      ).addTo(map);
     } catch (error) {
-       Swal.fire("Please enter a valid ip address.");
-       document.querySelector(".ipAddDetail").innerHTML=""
-       document.querySelector(".locationDetail").innerHTML=""
-       document.querySelector(".timezoneDetail").innerHTML=""
-       document.querySelector(".ispDetail").innerHTML=""
+      Swal.fire("Please enter a valid ip address.");
+      document.querySelector(".ipAddDetail").innerHTML = "";
+    document.querySelector(".locationDetail").innerHTML ="";
+    document.querySelector(".timezoneDetail").innerHTML ="";
+    document.querySelector(".ispDetail").innerHTML ="";
     }
-    
   };
   useEffect(() => {
     getData();
@@ -100,18 +100,18 @@ const Home = () => {
   }, []);
   return (
     <div>
-        <div className="header">
-      <h1> IP Address Tracker</h1>
-      <div className="btnBox">
-      <input
-        type="text"
-        placeholder="Search for any IP address"
-        className="ipAddInput"
-      />
-      <button className="searchBtn" onClick={handleClick}>
-        <img src={arrowIcon}/>
-      </button>
-      </div>
+      <div className="header">
+        <h1> IP Address Tracker</h1>
+        <div className="btnBox">
+          <input
+            type="text"
+            placeholder="Search for any IP address"
+            className="ipAddInput"
+          />
+          <button className="searchBtn" onClick={handleClick}>
+            <img src={arrowIcon} />
+          </button>
+        </div>
       </div>
       <div className="details">
         <div className="ipAddress">
